3cce1cd9d245ccefa60616db904903bf8a067c58
stndrk <satendra.km@alumni.iitd.ac.in>
2026-03-26 08:09:36 +0530

    add environment varibles

--- hrms_backend/settings.py
+++ hrms_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o(msqb&!)5&-j_(wm&kyr3&+3gs9m#$^zhk_ze8ft7pn&v5cy6'
+SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
